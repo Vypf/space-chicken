950ebdd06a49aa7f2200e00811e90358ce5dba07
Vincent POESMANN <vincent.poesmann@gmail.com>
2025-10-10 16:34:31 +0200

    Take review into account
    - Move detection logic to Pickable
    - Add collision layer/mask initialisation to Pickable
    - Add interact abstract function
    - Refactor animated sprite variable

--- pickups/pickable.gd
+++ pickups/pickable.gd
@@ -1,6 +1,17 @@
 @abstract
 class_name Pickable extends Area2D
 
+func _ready():
+	# Layer 3 : Pickable (0010...)
+	collision_layer = 4
+	# Layer 1 : Player (1000...)
+	collision_mask = 1
+	body_entered.connect(_on_body_entered)
+	
 
 @abstract
-func _on_body_entered(player: Node2D)
+func interact(player: CharacterBody2D)
+
+func _on_body_entered(player: Node2D):
+	if player is CharacterBody2D:
+		interact(player)
